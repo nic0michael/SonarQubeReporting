69f2806961714c3e5e5d49d0547f07d00c5f6a1d
Nico Michael <nic0michael@users.noreply.github.com>
2021-07-14 11:02:59 +0200

    added class

---
+++ src/main/java/za/co/nico/poc/dtos/json/Component.java
@@ -0,0 +1,85 @@
+package za.co.nico.poc.dtos.json;
+
+import java.util.List;
+
+public class Component {
+    String organization;
+    String id;
+    String  key;
+    String name;
+    boolean isFavorite;
+    List<String> tags;
+    String visibility;
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(String visibility) {
+        this.visibility = visibility;
+    }
+
+    @Override
+    public String toString() {
+        return "Component{" +
+                "organization='" + organization + '\'' +
+                ", id='" + id + '\'' +
+                ", key='" + key + '\'' +
+                ", name='" + name + '\'' +
+                ", isFavorite=" + isFavorite +
+                ", tags=" + tags +
+                ", visibility='" + visibility + '\'' +
+                '}';
+    }
+}
+
+
+
